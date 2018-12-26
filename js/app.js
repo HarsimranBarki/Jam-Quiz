4946d2398a7e8db5aa0ddc81e475b15b7be8a59d
Harsimran <harsimransinghbarki@gmail.com>
2018-12-26 22:15:43 +0530

    Added Functionality And Animation

--- js/app.js
+++ js/app.js
@@ -1,19 +1,110 @@
 // DOM
-let buttonOne = document.getElementById('one');
-let buttonTwo = document.getElementById('two');
-let buttonThree = document.getElementById('three');
-let buttonFour = document.getElementById('four');
+const buttonOne = document.getElementById('one');
+const buttonTwo = document.getElementById('two');
+const buttonThree = document.getElementById('three');
+const buttonFour = document.getElementById('four');
 
-let questionAsked = document.querySelector('.main__question');
+const questionDisplayed = document.querySelector('.main__question');
+const answerDisplayed = document.querySelector('.main__answer');
 
-let questions = [
-    {
-        question: "Grand Central Terminal, Park Avenue, New York is the world's",
-        optionOne: "largest railway station",
-        optionTwo: "highest railway station",
-        optionThree: "longest railway station",
-        optionFour: "None of the above",
-        answer: "largest railway station"
+const main = document.querySelector('.main');
+const options = document.querySelector('.main__options');
+
+
+
+const gameState = {
+    score: 0,
+    current: 0,
+    isDone: false,
+    isWin: false,
+    total: quizQuestions.length,
+    displayQuestions(current) {
+        questionDisplayed.innerHTML = quizQuestions[current].question;
+    },
+    displayOptions(current) {
+        buttonOne.innerHTML = quizQuestions[current].answers.a;
+        buttonTwo.innerHTML = quizQuestions[current].answers.b;
+        buttonThree.innerHTML = quizQuestions[current].answers.c;
+        buttonFour.innerHTML = quizQuestions[current].answers.d;
+    },
+    displayAnswer(isWin) {
+        if (isWin === true) {
+            answerDisplayed.innerHTML = "You Are Correct!";
+            answerDisplayed.style.color = "#2ECC40";
+            this.current++;
+            this.score++;
+            questionDisplayed.classList.remove("animated", "fadeIn", "fast");
+            options.classList.remove("animated", "fadeIn", "fast");
+            setTimeout(() => {
+                runGame(this.current);
+                answerDisplayed.innerHTML = "";
+
+            }, 1000);
+        } else if (isWin === false) {
+            answerDisplayed.innerHTML = "You Are Incorrect!";
+            answerDisplayed.style.color = "#E53A40";
+            this.current++;
+            questionDisplayed.classList.remove("animated", "fadeIn", "fast");
+            options.classList.remove("animated", "fadeIn", "fast");
+            setTimeout(() => {
+                runGame(this.current);
+                answerDisplayed.innerHTML = "";
+            }, 1000);
+        }
     }
-];
+}
 
+function runGame(current) {
+    questionDisplayed.classList.add("animated", "fadeIn", "fast");
+    options.classList.add("animated", "fadeIn", "fast");
+    if (current < gameState.total) {
+        gameState.displayOptions(current);
+        gameState.displayQuestions(current);
+    } else {
+        main.innerHTML = `<div>
+        <h1 id="game">Game Over</h1>
+        <h1 id="score">Score: ${gameState.score} </h1>
+        <button id="restart" onclick = "restart()">Restart</button>
+        </div>`;
+    }
+}
+
+runGame(0);
+
+
+let nowScore = gameState.current;
+
+buttonOne.addEventListener('click', () => {
+    if (buttonOne.textContent === quizQuestions[gameState.current].correctAnswer) {
+        gameState.displayAnswer(true);
+
+    } else {
+        gameState.displayAnswer(false);
+    }
+});
+buttonTwo.addEventListener('click', () => {
+    if (buttonTwo.textContent === quizQuestions[gameState.current].correctAnswer) {
+        gameState.displayAnswer(true);
+    } else {
+        gameState.displayAnswer(false);
+    }
+});
+buttonThree.addEventListener('click', () => {
+    if (buttonThree.textContent === quizQuestions[gameState.current].correctAnswer) {
+        gameState.displayAnswer(true);
+    } else {
+        gameState.displayAnswer(false);
+    }
+});
+buttonFour.addEventListener('click', () => {
+    if (buttonFour.textContent === quizQuestions[gameState.current].correctAnswer) {
+        gameState.displayAnswer(true);
+    } else {
+        gameState.displayAnswer(false);
+    }
+});
+
+
+function restart() {
+    window.location.reload();
+}
---
+++ js/data.js
@@ -0,0 +1,73 @@
+
+const quizQuestions = [
+    {
+        question: "Entomology is the science that studies",
+        answers: {
+            a: "Behavior of human beings",
+            b: "Insects",
+            c: "The origin and history of technical and scientific terms",
+            d: "The formation of rocks"
+        },
+        correctAnswer: "Insects"
+    },
+    {
+        question: "Eritrea, which became the 182nd member of the UN in 1993, is in the continent of",
+        answers: {
+            a: "Asia",
+            b: "Africa",
+            c: "Europe",
+            d: "Australia"
+        },
+        correctAnswer: "Africa"
+    },
+    {
+        question: "Garampani sanctuary is located at",
+        answers: {
+            a: "Junagarh, Gujarat",
+            b: "Diphu, Assam",
+            c: "Kohima, Nagaland",
+            d: "Gangtok, Sikkim"
+        },
+        correctAnswer: "Diphu, Assam"
+    },
+    {
+        question: "For which of the following disciplines is Nobel Prize awarded?",
+        answers: {
+            a: "Physics and Chemistry",
+            b: "Physiology or Medicine",
+            c: "Literature, Peace and Economics",
+            d: "All of the above"
+        },
+        correctAnswer: "All of the above"
+    },
+    {
+        question: "FFC stands for",
+        answers: {
+            a: "Foreign Finance Corporation",
+            b: "Film Finance Corporation",
+            c: "Federation of Football Council",
+            d: "None of the above"
+        },
+        correctAnswer: "Film Finance Corporation"
+    },
+    {
+        question: "Hitler party which came into power in 1933 is known as",
+        answers: {
+            a: "Labour Party",
+            b: "Nazi Party",
+            c: "Ku-Klux-Klan",
+            d: "Democratic Party"
+        },
+        correctAnswer: "Nazi Party"
+    },
+    {
+        question: "Fastest shorthand writer was",
+        answers: {
+            a: "Dr. G. D. Bist",
+            b: "J.R.D. Tata",
+            c: "J.M. Tagore",
+            d: "Khudada Khan"
+        },
+        correctAnswer: "Dr. G. D. Bist"
+    }
+]
