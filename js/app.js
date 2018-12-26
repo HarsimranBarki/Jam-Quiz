2e211ab2bd1c09af5327f66897fa3f2fe42b1f56
Harsimran <harsimransinghbarki@gmail.com>
2018-12-26 16:57:15 +0530

    added Styling Question and answer buttons

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// DOM
+let buttonOne = document.getElementById('one');
+let buttonTwo = document.getElementById('two');
+let buttonThree = document.getElementById('three');
+let buttonFour = document.getElementById('four');
+
+let questionAsked = document.querySelector('.main__question');
+
+let questions = [
+    {
+        question: "Grand Central Terminal, Park Avenue, New York is the world's",
+        optionOne: "largest railway station",
+        optionTwo: "highest railway station",
+        optionThree: "longest railway station",
+        optionFour: "None of the above",
+        answer: "largest railway station"
+    }
+];
+
